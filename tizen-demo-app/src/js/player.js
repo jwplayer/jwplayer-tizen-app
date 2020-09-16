29a1261bf9679cd273820abedc8eda54d268f08f
Maria Maddox <mmaddox@jwplayer.com>
2020-09-16 16:01:42 -0400

    Add tizen error state

--- tizen-demo-app/src/js/player.js
+++ tizen-demo-app/src/js/player.js
@@ -1,31 +1,24 @@
+import ErrorState from './error';
+
 /* eslint-disable no-undef */
 export default function(config, prevPageCallback) {
     const playerDiv = document.createElement('div');
     playerDiv.id = 'tizen-player';
 
     const mainDiv = document.querySelector('#main');
-    mainDiv.removeChild(mainDiv.firstElementChild);
-    mainDiv.appendChild(playerDiv);
-
-    const handleKeydown = (event) => {
-        switch(event.keyCode) {
-            case 10009: // Back/Return
-                if (jwplayer().getState() === 'error') {
-                    onBackClick();
-                }
-                break;
-            default:
-                break;
-        }
+    if (mainDiv.firstElementChild) {
+        mainDiv.removeChild(mainDiv.firstElementChild);
     }
+    mainDiv.appendChild(playerDiv);
 
     jwplayer('tizen-player').setup(config);
 
+    jwplayer().on('error', () => ErrorState(prevPageCallback));
+    jwplayer().on('setupError', () => ErrorState(prevPageCallback));
+
     jwplayer().on('backClick', () => onBackClick());
-    document.addEventListener('keydown', handleKeydown);
 
     function onBackClick() {
         prevPageCallback();
-        document.removeEventListener('keydown', handleKeydown);
     }
 }
