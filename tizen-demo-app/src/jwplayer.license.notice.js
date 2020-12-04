02e5fbba59f6e7e893bd033221f53239777d016c
Maria Maddox <mmaddox@jwplayer.com>
2020-12-04 13:58:50 -0500

    Add source headers

---
+++ tizen-demo-app/src/jwplayer.license.notice.js
@@ -0,0 +1,11 @@
+const notice = `
+*
+* Copyright (c) 2020, JW Player, All Rights Reserved 
+*
+* This source code and its use and distribution, is subject to the terms
+* and conditions of the applicable license agreement.
+* https://www.jwplayer.com/tos/
+*
+`
+
+module.exports = notice;
