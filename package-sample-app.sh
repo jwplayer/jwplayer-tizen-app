0207080b3ad2b3fd5af4202ab3dc27cebd3bd163
Jon-Carlos Rivera <jon.carlos.rivera@gmail.com>
2023-10-04 11:18:50 -0700

    Make the package command a little better

--- package-sample-app.sh
+++ package-sample-app.sh
@@ -1,4 +1,8 @@
 #! /bin/sh
 
+ROOT_DIR="jwplayer-tizen-demo"
+
 # Package just the app directory, exclude any `.sign` files because they are private
-zip -r sample-app.zip tizen-demo-app/app -9 -x tizen-demo-app/app/.sign/\*
+ln -s tizen-demo-app/app $ROOT_DIR
+zip -r $ROOT_DIR.zip $ROOT_DIR -9 -x $ROOT_DIR/.sign/\* $ROOT_DIR/\*.wgt
+unlink $ROOT_DIR
